01a3d4e88eb6061a3c0976eba54a6c2d39536cc4
thejjwang <jonathanjacobwang@gmail.com>
2023-09-02 23:35:30 -0700

    need to fix input to add

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -22,11 +22,34 @@ import ShowCart from './ShowCart';
 
 function App() {
   const [cart, setCart] = useState([]);
+  const [userItem, setUserItem] = useState([]);
+  const [userPrice, setUserPrice] = useState([]);
 
+  // post 
+  const fetchProducts = async () => {
+    try {
+        let response = await fetch('http://localhost:3000/products', {
+          method: "POST",
+
+        });
+        let data = await response.json();
+        setProducts(data);
+    } catch (error){
+        console.log("error fetching products:" + error)
+    }
+}
+fetchProducts()
+ 
   return (
     <>
       <div className='header'>
         <h1>Shopping List</h1>
+      </div>
+      <div className='addItemContainer'>
+        <input onChange={(e)=> setUserItem(e.target.value)} value={userItem} placeholder='Enter Item' />
+        <input onChange={(e)=> setUserPrice(e.target.value)} value={userPrice} placeholder='Enter Price' />
+        {/* need to make it so button fetch posts the new item and price */}
+        <button onClick={()=>console.log("")}>Add Item</button>
       </div>
       <ProductList setCart={setCart}/>
       <ShowCart cart={cart}/>
